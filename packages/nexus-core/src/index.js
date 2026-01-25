0321771fcb948190b1e91eda5f92068b8e5299c6
Todd Morgan <you@example.com>
2026-01-25 17:45:44 -0500

    chore(build): stop emitting nexus-core outputs into src; ignore dist artifacts

--- packages/nexus-core/src/index.js
+++
@@ -1,4 +0,0 @@
-export { defaultRoutingPolicy } from "./orchestration.js";
-export { NexusOrchestrator } from "./orchestrator_v1.js";
-// HANDSHAKE_EXPORTS_EXPLICIT
-//# sourceMappingURL=index.js.map
